80cf9f7649a225b7c7d808a5eec6000d62e9cc6c
Vaishnavi Kadam <vaishnavi.kadam413@gmail.com>
2023-09-25 22:55:47 +0530

    Routing with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -2,29 +2,18 @@ import './App.css';
 import Header from './Header';
 import Post from './Post';
 import {Route, Routes} from "react-router-dom";
+import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
 
     <Routes>
-      <Route index element={
-        <main>
-        <Header />
-  
-        <Post />
-        <Post />
-        <Post />
-  
-      </main>
-      } />
-      <Route path={'/login'} element={
-        <main>
-          <Header />
-          <div>login page</div>
-        </main>
-        
-      }
-      />
+      <Route path='/' element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path='/login' element={<LoginPage />}/>
+      </Route>
     </Routes>
 
     

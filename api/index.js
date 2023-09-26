a46a33f74a4ec89d3f95b71053b8cd061aed6eb1
Vaishnavi Kadam <vaishnavi.kadam413@gmail.com>
2023-09-27 01:18:25 +0530

    runtime err

--- api/index.js
+++ api/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+
 const app = express();
+const salt = bcrypt.genSaltSync(10);
 
 
 app.use(cors());
@@ -13,9 +16,24 @@ mongoose.connect('mongodb+srv://blog:pEYdA9Z5Ub9D2Qb5@cluster0.iky9irj.mongodb.n
 
 app.post('/register', async (req, res) => {
     const {username, password} = req.body;
-    const userDoc = await User.create({ username, password });
-    res.json(userDoc);
+    try{
+        const userDoc = await User.create({ 
+            username, 
+            password:bcrypt.hashSync(password, salt),
+        });
+        res.json(userDoc);
+    } catch(e){
+        res.status(400).json(e);
+    }
 })
+
+
+app.post('/login', async (req,res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    res.json(passOk);
+});
 
 
 app.listen(4000);
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
+
 export default function LoginPage() {
+    const [username,SetUsername] = useState('');
+    const [password, SetPassword] = useState('');
+
+    async function login(ev) {
+        ev.preventDefault();
+        await fetch('https://localhots:4000/login', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-Type':'application/json'},
+        });
+    }
+
     return (
-        <form className="login">
+        <form className="login" onSubmit={ login }>
             <h1>Login</h1>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
+            <input type="text" 
+                placeholder="username" 
+                value={username} 
+                onChange={ev => SetUsername(ev.target.value)} />
+            <input type="password" 
+                placeholder="password" 
+                value={password}
+                onChange={ev => SetPassword(ev.target.value)} />
             <button>Login</button>
         </form>
     );
